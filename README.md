ed49d7884680e6c066ec73b8d14b4a0d3fe82c8c
Olivier Wilkinson <olivier.wilkinson@gmail.com>
2022-09-20 16:52:59 +0100

    feat: initial release

---
+++ README.md
@@ -0,0 +1,116 @@
+<div align="center">
+<h1>Prisma Nested Middleware</h1>
+
+<p>Util for calling Prisma middleware for nested write operations.</p>
+
+<p>Existing Prisma middleware is called once for every operation, but due to some operations containing nested write operations it can become complex to ensure middleware is applied in all scenarios. See the existing <a href="https://github.com/prisma/prisma/issues/4211">issue regarding nested middleware</a> for more information.  
+
+</div>
+
+<hr />
+
+[![Build Status][build-badge]][build]
+[![version][version-badge]][package]
+[![MIT License][license-badge]][license]
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+
+[![PRs Welcome][prs-badge]][prs]
+
+## Table of Contents
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [LICENSE](#license)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+## Installation
+
+This module is distributed via [npm][npm] which is bundled with [node][node] and
+should be installed as one of your project's dependencies:
+
+```
+npm install --save prisma-nested-middleware
+```
+
+## Usage
+
+To make nested middleware you pass a [middleware function](https://www.prisma.io/docs/concepts/components/prisma-client/middleware) to `createNestedMiddleware` which returns modified middleware to be passed to Prisma client's `$use` method:
+
+```javascript
+import { createNestedMiddlware } from 'prisma-nested-middleware'
+
+client.$use(createNestedMiddleware((params, next) => {
+  // update params here
+  const result = await next(params)
+  // update result here
+  return result;
+));
+```
+
+Middleware passed to `createNestedMiddleware` is called for every nested relation.
+
+There are some differences to note when using nested middleware:
+- the list of actions that might be in params is expanded to include 'connectOrCreate'
+- If a relation is not included using `include` then that middleware's `next` function will resolve with `undefined`.
+- sometimes the parent params are relevent, for instance if being connected you need to know the parent you are being connected to. To resolve this I added a `scope` object to params of nested middleware which is the parent params.
+- when handling nested `create` actions `params.args` does not include a `data` field, that must be handled manually. You can use the existence of `params.scope` to know when to handle a nested `create`.
+
+It is helpful to walk through the lifecycle of an operation:
+
+For the following update
+
+```javascript
+client.country.update({
+  where: { id: 'imagination-land' },
+  data: {
+    nationalDish: {
+      update: {
+        where: { id: 'stardust-pie' },
+        data: {
+          keyIngredient: {
+            connectOrCreate: {
+              create: { name: 'Stardust' },
+              connect: { id: 'stardust' },
+            },
+          },
+        },
+      },
+    },
+  },
+});
+```
+
+`createNestedMiddleware` calls the passed middleware function with params in the following order:
+1. `{ model: 'Recipe', action: 'update', args: { where: { id: 'stardust-pie' }, data: {...} } }`
+2. `{ model: 'Food', action: 'connectOrCreate', args: { create: {...}, connect: {...} } }`
+3. `{ model: 'Country', action: 'update', args: { where: { id: 'imagination-land', data: {...} } }`
+
+Then it waits for all the nested `next` functions to have been passed params, updates the top level params object with those objects and awaits the top level `next` function, in this case the `next` where model is 'Country'.
+
+Once the top level `next` function resolves with a result the `next` functions of the nested middleware are resolved with the slice of the result relevent to them. So the middleware called for the 'Recipe' model receives the recipe object, the middleware for the 'Food' receives the food object.
+
+Then the return values from the nested middleware are used to modify the top level result that is finally returned from the top level middleware.
+
+## LICENSE
+
+Apache 2.0
+
+[npm]: https://www.npmjs.com/
+[node]: https://nodejs.org
+[build-badge]: https://github.com/olivierwilkinson/prisma-nested-middleware/workflows/prisma-nested-middleware/badge.svg
+[build]: https://github.com/olivierwilkinson/prisma-nested-middleware/actions?query=branch%3Amaster+workflow%3Aprisma-nested-middleware
+[version-badge]: https://img.shields.io/npm/v/olivierwilkinson/prisma-nested-middleware.svg?style=flat-square
+[package]: https://www.npmjs.com/package/prisma-nested-middleware
+[downloads-badge]:https://img.shields.io/npm/dm/prisma-nested-middleware.svg?style=flat-square
+[npmtrends]: http://www.npmtrends.com/prisma-nested-middleware
+[license-badge]: https://img.shields.io/npm/l/prisma-nested-middleware.svg?style=flat-square
+[license]: https://github.com/olivierwilkinson/prisma-nested-middleware/blob/master/LICENSE
+[prs-badge]: https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square
+[prs]: http://makeapullrequest.com
+[coc-badge]: https://img.shields.io/badge/code%20of-conduct-ff69b4.svg?style=flat-square
+[coc]: https://github.com/olivierwilkinson/prisma-nested-middleware/blob/master/other/CODE_OF_CONDUCT.md
---
+++ src/index.ts
@@ -0,0 +1,236 @@
+/* eslint-disable import/no-unresolved */
+// @ts-expect-error unable to generate prisma client before building
+import { Prisma } from '@prisma/client';
+
+import get from 'lodash/get';
+import set from 'lodash/set';
+
+if (!Prisma.dmmf) {
+  throw new Error('Prisma DMMF not found, please generate Prisma client using `npx prisma generate`');
+}
+
+type Field = {
+  kind:  "scalar" | "object" | "enum" | "list";
+  relationName?: string;
+  name: string;
+  type: string;
+}
+
+type Model = {
+  name: string;
+  fields: Field[];
+}
+
+const relationsByModel: Record<string, Field[]> = {};
+Prisma.dmmf.datamodel.models.forEach((model: Model) => {
+  relationsByModel[model.name] = model.fields.filter(
+    (field) => field.kind === 'object' && field.relationName
+  );
+});
+
+export type NestedAction = Prisma.PrismaAction | 'connectOrCreate';
+
+export type NestedParams = Omit<Prisma.MiddlewareParams, 'action'> & {
+  action: NestedAction;
+  scope?: NestedParams;
+};
+
+export type NestedMiddleware<T = any> = (
+  params: NestedParams,
+  next: (modifiedParams: NestedParams) => Promise<T>
+) => Promise<T>;
+
+type WriteInfo = {
+  params: NestedParams;
+  argPath: string;
+};
+
+type PromiseCallbackRef = {
+  resolve: (result?: any) => void;
+  reject: (reason?: any) => void;
+};
+
+const writeOperationsSupportingNestedWrites: NestedAction[] = [
+  'create',
+  'update',
+  'upsert',
+  'connectOrCreate',
+];
+
+const writeOperations: NestedAction[] = [
+  ...writeOperationsSupportingNestedWrites,
+  'createMany',
+  'updateMany',
+  'delete',
+  'deleteMany',
+];
+
+function isWriteOperation(key: any): key is NestedAction {
+  return writeOperations.includes(key);
+}
+
+function extractWriteInfo(
+  params: NestedParams,
+  model: string,
+  argPath: string
+): WriteInfo[] {
+  const arg = get(params.args, argPath, {});
+
+  return Object.keys(arg)
+    .filter(isWriteOperation)
+    .map((operation) => ({
+      argPath,
+      params: {
+        ...params,
+        model,
+        action: operation,
+        args: arg[operation],
+        scope: params,
+      },
+    }));
+}
+
+function extractNestedWriteInfo(
+  params: NestedParams,
+  relation: Field
+): WriteInfo[] {
+  const model = relation.type;
+
+  switch (params.action) {
+    case 'upsert':
+      return [
+        ...extractWriteInfo(params, model, `update.${relation.name}`),
+        ...extractWriteInfo(params, model, `create.${relation.name}`),
+      ];
+
+    case 'create':
+      // nested creates use args as data instead of including a data field.
+      if (params.scope) {
+        return extractWriteInfo(params, model, relation.name);
+      }
+
+      return extractWriteInfo(params, model, `data.${relation.name}`);
+
+    case 'update':
+    case 'updateMany':
+    case 'createMany':
+      return extractWriteInfo(params, model, `data.${relation.name}`);
+
+    case 'connectOrCreate':
+      return extractWriteInfo(params, model, `create.${relation.name}`);
+
+    default:
+      return [];
+  }
+}
+
+export function createNestedMiddleware<T>(
+  middleware: NestedMiddleware
+): Prisma.Middleware<T> {
+  const nestedMiddleware: NestedMiddleware = async (params, next) => {
+    const relations = relationsByModel[params.model || ''] || [];
+    const finalParams = params;
+    const nestedWrites: {
+      relationName: string;
+      nextReached: Promise<unknown>;
+      resultCallbacks: PromiseCallbackRef;
+      result: Promise<any>;
+    }[] = [];
+
+    if (writeOperationsSupportingNestedWrites.includes(params.action)) {
+      relations.forEach((relation) =>
+        extractNestedWriteInfo(params, relation).forEach((nestedWriteInfo) => {
+          // store nextReached promise callbacks to set whether next has been
+          // called or if middleware has thrown beforehand
+          const nextReachedCallbacks: PromiseCallbackRef = {
+            resolve() {},
+            reject() {},
+          };
+
+          // store result promise callbacks so we can settle it once we know how
+          const resultCallbacks: PromiseCallbackRef = {
+            resolve() {},
+            reject() {},
+          };
+
+          // wrap params updated callback in a promise so we can await it
+          const nextReached = new Promise<void>((resolve, reject) => {
+            nextReachedCallbacks.resolve = resolve;
+            nextReachedCallbacks.reject = reject;
+          });
+
+          const result = nestedMiddleware(
+            nestedWriteInfo.params,
+            (updatedParams) => {
+              // Update final params to include nested middleware changes.
+              // Scope updates to [argPath].[action] to avoid breaking params
+              set(
+                finalParams.args,
+                `${nestedWriteInfo.argPath}.${updatedParams.action}`,
+                updatedParams.args
+              );
+
+              // notify parent middleware that params have been updated
+              nextReachedCallbacks.resolve();
+
+              // only resolve nested next when resolveRef.resolve is called
+              return new Promise((resolve, reject) => {
+                resultCallbacks.resolve = resolve;
+                resultCallbacks.reject = reject;
+              });
+            }
+          ).catch((e) => {
+            // reject nextReached promise so if it has not already resolved the
+            // parent will catch the error when awaiting it.
+            nextReachedCallbacks.reject(e);
+
+            // rethrow error so the parent catches it when awaiting `result`
+            throw e;
+          });
+
+          nestedWrites.push({
+            relationName: relation.name,
+            nextReached,
+            resultCallbacks,
+            result,
+          });
+        })
+      );
+    }
+
+    try {
+      // wait for all nested middleware to have reached next and updated params
+      await Promise.all(nestedWrites.map(({ nextReached }) => nextReached));
+
+      // evaluate result from parent middleware
+      const result = await middleware(finalParams, next);
+
+      // resolve nested middleware next functions with relevant slice of result
+      await Promise.all(
+        nestedWrites.map(async (nestedWrite) => {
+          // result cannot be null because only writes can have nested writes.
+          const nestedResult = get(result, nestedWrite.relationName);
+
+          // if relationship hasn't been included nestedResult is undefined.
+          nestedWrite.resultCallbacks.resolve(nestedResult);
+
+          // set final result relation to be result of nested middleware
+          set(result, nestedWrite.relationName, await nestedWrite.result);
+        })
+      );
+
+      return result;
+    } catch (e) {
+      // When parent rejects also reject the nested next functions promises
+      await Promise.all(
+        nestedWrites.map((nestedWrite) => {
+          nestedWrite.resultCallbacks.reject(e);
+          return nestedWrite.result;
+        })
+      );
+      throw e;
+    }
+  };
+
+  return (nestedMiddleware as unknown) as Prisma.Middleware;
+}
